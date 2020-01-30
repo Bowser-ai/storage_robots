3189649f1d7f48e33bae423886c0b92e525f6b3b
Martijn <martijnvermeulen1@gmail.com>
2020-01-30 09:41:54 +0100

    robot finished 30_01_9_41

--- Robot.js
+++ Robot.js
@@ -1,3 +1,6 @@
+
+const selectComperator = Symbol();
+
 class Robot
 {
     constructor(name, initial_coord = new Point(0, 0))
@@ -18,10 +21,16 @@ class Robot
         this.signalReady();
     }
 
+    /*=========================================================================
+     *                          PUBLIC
+     *========================================================================*/
+
     getName()
     {
         return this.name;
     }
+
+ /*===========================================================================*/
 
     go()
     {
@@ -32,12 +41,15 @@ class Robot
                 this.addAssignment(current_assignment);
             } 
 
+            // nothing to do?? signal ready!!
             else 
             {
                 this.ready = true;
             }
         }
 
+        // no green ligth means the robot will posts its intensions to the
+        // platform
         if(!this.green_light)
         {
             const pair_current_next = new Move(
@@ -45,16 +57,25 @@ class Robot
                 this.route.shift()
             );
             //this.platform.post(this, pair_current_next);
+            
+        }
+        else {
+
+            //we have green light, pop the next coord from the route and go
             this.route.shift();
             this.green_light = false;
         }
     }
 
+ /*===========================================================================*/
+
     signalReady()
     {
         this.ready = true;
-        //platform.postReady();
+        //platform.postReady(this);
     }
+
+ /*===========================================================================*/
 
     giveGreenLight()
     {
@@ -62,10 +83,14 @@ class Robot
         this.go();
     }
 
+ /*===========================================================================*/
+
     getCurrentLocation()
     {
         return this.current_coord;
     }
+
+ /*===========================================================================*/
 
     addAssignment(assignment)
     {
@@ -76,51 +101,63 @@ class Robot
         //this.go();
     }
 
-    deltaXdeltaY(p0_dimension, p1_dimension)
-    {
-        let compare_fn = {};
-
-        if (p0_dimension < p1_dimension)
-        {
-            compare_fn = (p0_dimension, p1_dimension) => 
-            {
-                return p0_dimension <= p1_dimension;
-            };
-        }
-        else
-        {
-            compare_fn = (p0_dimension, p1_dimension) =>
-            {
-                return p0_dimension >= p1_dimension
-            }
-        }
-
-        return compare_fn;
-    }
+ /*===========================================================================*/
 
     calculateRoute()
     {
         const p0 = this.getCurrentLocation();
         const p1 = this.current_assignment.popFirst();
 
-        const compX = this.deltaXdeltaY(p0.x, p1.x);
-        const compY = this.deltaXdeltaY(p0.y, p1.y);
+        const compX = this[selectComperator](p0.x, p1.x);
+        const compY = this[selectComperator](p0.y, p1.y);
+
+
+        // if the start coord is lower than the target coord, we need to
+        // increase the coord every step, else we need to decrease the coord
+        const increaseOrDecrease = (p0_dimension, p1_dimension, dimension) =>
+        {
+            if (p0_dimension < p1_dimension) return ++dimension;
+            else                             return --dimension;
+        };
 
         let x = p0.x;
         let y = p0.y + 1;
 
         while(compX(x, p1.x))
         {
+            x = increaseOrDecrease(p0.x, p1.x, x);
             this.route.push(new Point(x, p0.y));
-            if (p0.x < p1.x) ++x;
-            else             --x;
         }
 
         while(compY(y, p1.y))
         {
+            y = increaseOrDecrease(p0.y, p1.y, y);
             this.route.push(new Point(p1.x, y));
-            if (p0.y < p1.y) ++y;
-            else             --y;
         }
     }
+
+ /*===========================================================================*/
+
+    // function seletect the correct comperator, if the start coord is lower
+    // than the target coord, then antother comperator has to be chosen
+    
+    [selectComperator](p0_dimension, p1_dimension)
+    {
+        let compare_fn = {};
+
+        if (p0_dimension < p1_dimension)
+        {
+            compare_fn = (p0_dimension, p1_dimension) => 
+                p0_dimension <= p1_dimension;
+        }
+        else
+        {
+            compare_fn = (p0_dimension, p1_dimension) => 
+                p0_dimension >= p1_dimension
+        }
+
+        return compare_fn;
+    }
+
+ /*===========================================================================*/
 }
